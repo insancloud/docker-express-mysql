2cfd6437487385f37b7ed4c7ff3c1ae71de6d1ef
insancloud <insan5272@icloud.com>
2023-09-07 16:35:13 +0800

    express MYSQL

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+
+// Constants
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+// Create a route to fetch data from the database and send it to the client
+app.get('/get-users', (req, res) => {
+  const connection = mysql.createConnection({
+    host: 'db', // Assuming the database is running in a container named 'db'
+    user: 'root',
+    password: 'barbar',
+    database: 'express-db'
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * FROM users', (err, rows, fields) => {
+    if (err) {
+      console.error('Error querying the database:', err);
+      res.status(500).send('Error querying the database');
+      return;
+    }
+
+    // Send the database results to the client as JSON
+    res.json(rows);
+  });
+
+  connection.end();
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
